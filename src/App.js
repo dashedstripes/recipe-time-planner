bb9abb3661f31133635f7b6706c2216c582d13b2
Adam Gray <adam@pateo.co>
2018-05-24 17:54:26 +0100

    Remove 'useless constructor'

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import IngredientsList from './containers/IngredientsList';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return (
       <div>
