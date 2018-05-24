ec223bfa69590cbe9d840da355614e08153df96b
Adam Gray <adam@pateo.co>
2018-05-24 17:53:52 +0100

    Add header and intro copy

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import IngredientsList from './containers/IngredientsList';
 class App extends Component {
   constructor(props) {
     super(props)
-
-    this.state = {}
-
   }
 
   render() {
     return (
       <div>
+        <div>
+          <h1>Recipe Time Planner <small>by <a href='https://pateo.co'>pateo</a></small></h1>
+        </div>
+        <div>
+          <p>Have you ever struggled when cooking a complicated meal? Juggling multiple ingredients all with different prep and cook times can be a hassle. This app lets you add your ingredients to the panel on the right, set prep and/or cook times for each ingredient, it will then give you a step by step list including timings ending when you wish to serve the meal.</p>
+        </div>
         <IngredientsList />
       </div>
     );
