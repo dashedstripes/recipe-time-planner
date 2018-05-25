e6664975e136bc010322ccf7cdca6034e32dc655
Adam Gray <adam@pateo.co>
2018-05-25 21:18:43 +0100

    Some issues with prep and cook times to work out

--- src/utils/recipeTime.js
+++ src/utils/recipeTime.js
@@ -2,29 +2,29 @@ import moment from 'moment'
 import { TIMING_TYPES, TIMES } from '../utils/types'
 
 // This is the data object we want to generate from our state
-let data = {
-  steps: [
-    [
-      {
-        title: 'Roast Chicken',
-        type: TIMING_TYPES.PREP,
-        time: '18:15 PM'
-      },
-      {
-        title: 'Mash Potato',
-        type: TIMING_TYPES.PREP,
-        time: '18:15 PM'
-      }
-    ],
-    [
-      {
-        title: 'Roast Chicken',
-        type: TIMING_TYPES.COOK,
-        time: '18:30 PM'
-      }
-    ]
+let idealData = [
+  [
+    {
+      title: 'Roast Chicken',
+      type: TIMING_TYPES.PREP,
+      time: '18:15 PM'
+    },
+    {
+      title: 'Mash Potato',
+      type: TIMING_TYPES.PREP,
+      time: '18:15 PM'
+    }
+  ],
+  [
+    {
+      title: 'Roast Chicken',
+      type: TIMING_TYPES.COOK,
+      time: '18:30 PM'
+    }
   ]
-}
+]
+
+let data = []
 
 function getSuffix(value) {
   if (value === TIMES.AM) {
@@ -36,20 +36,22 @@ function getSuffix(value) {
   }
 }
 
-function getTime(target, timing) {
-  return moment(`${target.hour}:${target.minutes}:${getSuffix(target.time)}`, `h:mm A`)
+function getTime(time, timing) {
+  return moment(`${time}`, `h:mm A`)
     .subtract(timing.hours, 'hours')
     .subtract(timing.minutes, 'minutes')
     .format(`h:mm A`)
 }
 
+function getTargetTime(target) {
+  return moment(`${target.hour}:${target.minutes}:${getSuffix(target.time)}`, `h:mm A`)
+}
+
 function generateRecipeTime({ ingredients, target }) {
-  console.log(
-    getTime(
-      target,
-      ingredients[0].timings[0]
-    )
-  )
+  data = []
+
+
+  console.log(data)
 }
 
 export default generateRecipeTime
