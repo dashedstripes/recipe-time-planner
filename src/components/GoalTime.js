fd3810a2f47955e4919720c22416538e8307e85c
Adam Gray <adam@pateo.co>
2018-05-26 16:15:08 +0100

    Move goal time into own component

---
+++ src/components/GoalTime.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { TIMES } from '../utils/types'
+import Button from './Button'
+
+const GoalTime = ({ hour, minutes, time, onChange, onSubmit }) => (
+  <div>
+    <div className='col-12 border-bottom py-3'>
+      <p className='mb-0'>Have you ever struggled when cooking a complicated meal? Juggling multiple ingredients all with different prep and cook times can be a hassle. This app lets you add your ingredients to the panel on the right, set prep and/or cook times for each ingredient, it will then give you a step by step list including timings ending when you wish to serve the meal.</p>
+    </div>
+    <div className='col-12 py-3 text-center border-bottom'>
+      <p>What time would you like to dish up?</p>
+      <div className='form-group form-inline justify-content-center'>
+        <input className='form-control' type='number' name='hour' min={1} max={12} value={hour} onChange={onChange} />
+        <span>:</span>
+        <input className='form-control' type='number' name='minutes' min={0} max={59} value={minutes} onChange={onChange} />
+        <select className='form-control' name='time' value={time} onChange={onChange}>
+          <option value={TIMES.AM}>AM</option>
+          <option value={TIMES.PM}>PM</option>
+        </select>
+      </div>
+      <div>
+        <Button onClick={onSubmit}>VIEW YOUR RECIPE PLAN</Button>
+      </div>
+    </div>
+  </div>
+)
+
+export default GoalTime
--- src/containers/IngredientsList.js
+++ src/containers/IngredientsList.js
@@ -5,6 +5,7 @@ import Button from '../components/Button';
 
 import '../css/ingredient-list.css'
 import Ingredient from '../components/Ingredient';
+import GoalTime from '../components/GoalTime';
 
 // This is what the ingredients data object looks like
 // [
@@ -269,24 +270,13 @@ class IngredientsList extends Component {
       <div className='row'>
         <div className='col-6'>
           <div className='row'>
-            <div className='col-12 border-bottom py-3'>
-              <p className='mb-0'>Have you ever struggled when cooking a complicated meal? Juggling multiple ingredients all with different prep and cook times can be a hassle. This app lets you add your ingredients to the panel on the right, set prep and/or cook times for each ingredient, it will then give you a step by step list including timings ending when you wish to serve the meal.</p>
-            </div>
-            <div className='col-12 py-3 text-center border-bottom'>
-              <p>What time would you like to dish up?</p>
-              <div className='form-group form-inline justify-content-center'>
-                <input className='form-control' type='number' name='hour' min={1} max={12} value={this.state.target.hour} onChange={this.handleTarget} />
-                <span>:</span>
-                <input className='form-control' type='number' name='minutes' min={0} max={59} value={this.state.target.minutes} onChange={this.handleTarget} />
-                <select className='form-control' name='time' value={this.state.target.time} onChange={this.handleTarget}>
-                  <option value={TIMES.AM}>AM</option>
-                  <option value={TIMES.PM}>PM</option>
-                </select>
-              </div>
-              <div>
-                <Button onClick={this.handleSubmit}>VIEW YOUR RECIPE PLAN</Button>
-              </div>
-            </div>
+            <GoalTime
+              hour={this.state.target.hour}
+              minutes={this.state.target.minutes}
+              time={this.state.target.time}
+              onChange={this.handleTarget}
+              onSubmit={this.handleSubmit}
+            />
             <div className='col-12 py-3'>
               {recipePlan}
             </div>
